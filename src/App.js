bd48fc072b988c794e97137a877fab4f53877035
Arhan13 <arhanchoudhury@gmail.com>
2021-10-31 18:58:07 +0530

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 import Navbar from "./components/Navbar"
-import { Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import Staking from "./pages/Staking"
 import Stats from "./pages/Stats"
 import Profile from "./pages/Profile"
@@ -8,15 +8,17 @@ import About from "./pages/About"
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-			<Switch>
-				<Route path="/" component={Profile} exact />
-				<Route path="/about" component={About} exact />
-				<Route path="/manage" component={Staking} exact />
-				<Route path="/stats" component={Stats} exact />
-			</Switch>
-		</div>
+		<Router>
+			<div className="App">
+				<Navbar />
+				<Switch>
+					<Route path="/" component={Profile} exact />
+					<Route path="/about" component={About} exact />
+					<Route path="/manage" component={Staking} exact />
+					<Route path="/stats" component={Stats} exact />
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
